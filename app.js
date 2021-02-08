d1ee481f4b4e6aa2558db988831f8a8b401c1aa3
layla-lili <laylaisa.211@gmail.com>
2021-02-08 04:31:43 +0300

    get post delete create

--- app.js
+++ app.js
@@ -1,48 +1,78 @@
 const express = require("express");
-
+let cookies=require("./data");
 const app = express();
-const cors = require("cors");
+app.use(express.json());
+// const cors = require("cors");
+// app.use(cors());
+
+app.get("/", (req,res)=>{
+  res.status(200).json(cookies);
+});
+
+app.post("/", (req,res)=>{
+  req.body.id=cookies[cookies.length-1].id+1;
+  cookies.push(req.body);
+  if(cookieFound) res.status(200).json(cookies)
+  else
+  res.status(201).json({message:"created"});
+});
+
+app.get("/:cookieId", (req,res)=>{
+const {cookieId}=req.params;
+const cookieFound =cookies.find((cookie) => cookie.id === +cookieId)
+if(cookieFound){
+res.status(200).json(cookieFound);
+}else{
+    res.status(404).json({message: "cookie not found"});
+}
+});
 
 
-app.use(cors());
+app.delete("/:cookieId", (req,res)=>{
+  const {cookieId}=req.params;
+  const cookieFound =cookies.find((cookie) => cookie.id === +cookieId)
+  if(cookieFound){
+   cookies=cookies.filter((cookie)=> cookie !== cookieFound)
+  res.status(204).end();
+  }else{
+      res.status(404).json({message: "cookie not found"});
+  }
+  });
 
-const cookies = [
-  {
-    id: 1,
-    name: "Chocolate Chip Cookies",
-    slug: "chocolate-chip-cookies",
-    description: "Delicious cookie.. sold by dozen",
-    price: 15,
-    image:
-      "https://images-gmi-pmc.edge-generalmills.com/087d17eb-500e-4b26-abd1-4f9ffa96a2c6.jpg",
-  },
-  {
-    id: 2,
-    name: "Peanut Butter Cookies",
-    slug: "peanut-butter-cookies",
-    description: "Delicious cookie.. sold by dozen",
-    price: 3,
-    image:
-      "https://images-gmi-pmc.edge-generalmills.com/dcd4f799-7353-4e56-ba50-623581cba3bc.jpg",
-  },
-  {
-    id: 3,
-    name: "Salted Caramel Cookies",
-    slug: "salted-caramel-cookies",
-    description: "Delicious cookie.. sold by dozen",
-    price: 10,
-    image:
-      "https://images-gmi-pmc.edge-generalmills.com/586da0ed-8a79-4390-9137-f60852ca312a.jpg",
-  },
-];
 
-app.get("/products", (req, res) => {
-  console.log("HELLO");
-  res.json(cookies);
+
+const PORT=8000;
+app.listen(PORT, () => {
+  console.log(`The application is running on localhost:${PORT}`);
 });
 
 
 
-app.listen(8000, () => {
-  console.log("The application is running on localhost:8000");
-});
+
+// // app.get("/", (req, res) => {
+// //   console.log("HELLO");
+// //   res.json(cookies);
+// // });
+
+
+// app.get("/:cookieId", (req, res) => {
+//   const { cookieId }=req.params;
+//   const cookieFound = products.find((cookie) => cookie.id === +cookieId);
+//   if(cookieFound){
+//     res.status(200).json(cookieFound);
+//   }else{
+//     res.status(404).json({message:"cookie not found"})
+//   }
+ 
+// });
+
+// app.delete("/:cookieId", (req, res) => {
+//   const {cookieId}= req.params;
+//   const cookieFound = products.find((cookie) => cookie.id === +cookieId);
+//   if(cookieFound){
+//    cookies=cookies.filter((cookie)=> cookies.id !== cookieFound);
+   
+//   }else{
+//     res.status(404).json({message:"cookie not found"})
+//   }
+// });
