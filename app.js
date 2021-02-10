30c34388f839d953fa8f219549137944591456e3
layla-lili <laylaisa.211@gmail.com>
2021-02-10 17:29:33 +0300

    05-Models

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 let cookies=require("./data");
-
-app.use(express.json());
 //const db=require("./db/models/index");
 const db = require("./db/models");
-const {product} = require("./db/models/product");
+const {product} = require("./db/models/");
+
+app.use(express.json());
+
 const PORT=8000;
 db.sequelize.authenticate();
 
@@ -15,20 +16,26 @@ app.listen(PORT), ()=>{
 
 db.sequelize.sync();
 
-app.get("/", async(req,res)=>{
-  const products= await product.findAll()
-  // res.status(200).json(cookies);
-  res.status(200).json(products);
+app.get("/", async (req, res) => {
+  console.log(req.body);
+  try {
+    const products = await Product.findAll({ attributes: req.body });
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 app.post("/", async (req,res)=>{
   try {
 
-     const newcookie= await {
-  id: cookies[cookies.length-1].id+1,
-  ...req.body,
-  };
-  cookies.push(newcookie);
+    const newcookie= await Product.create(req.body);
+    res.status(201).json(newProduct);
+            // {
+          // id: cookies[cookies.length-1].id+1,
+          // ...req.body,
+          // };
+          // cookies.push(newcookie);
   res.status(201).json(newcookie);
   } 
  
--- db/models/product.js
+++ db/models/product.js
@@ -1,26 +1,25 @@
-const { Sequelize, DataTypes } = require('sequelize');
+const SequelizeSlugify = require("sequelize-slugify");
 
-const SequelizeSlugify=require("sequelize-slugify");
-
-module.exports=(sequelize,DataTypes)=>{
-    return sequelize.define("Product",{ //name of the model to export
-        name:{
-            type:DataTypes.STRING,
-        },
-        slug:{
-            type:DataTypes.STRING,
-        },
-        description:{
-            type:DataTypes.STRING,
-        },
-        price:{
-            type: DataTypes.FLOAT,
-        },
-        image:{
-            type:DataTypes.STRING,
-        }
-    });
-    SequelizeSlugify.slugifyModel(Product,{
-        source:["name"],
-    })
-}
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define("Product", {
+    name: {
+      type: DataTypes.STRING,
+    },
+    slug: {
+      type: DataTypes.STRING,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+    },
+    image: {
+      type: DataTypes.STRING,
+    },
+  });
+  SequelizeSlugify.slugifyModel(Product, {
+    source: ["name"],
+  });
+  return Product;
+};
